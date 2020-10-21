d0bd6f57cb5df5f00d9c2173a1be1a278d115a36
jeongjaein <wodls96@gmail.com>
2020-10-21 17:08:53 +0900

    Start Xcode project

---
+++ MannaDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MannaDemo
+//
+//  Created by 정재인 on 2020/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
