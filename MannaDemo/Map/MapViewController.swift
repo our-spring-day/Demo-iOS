91a730c77f56028373bb9f2325fc18ed81896745
s-doris <swdoriz@gmail.com>
2020-10-21 21:03:56 +0900

    feat : 디자인 버튼 추가 해봤음

--- MannaDemo/Map/MapViewController.swift
+++ MannaDemo/Map/MapViewController.swift
@@ -10,11 +10,44 @@ import NMapsMap
 
 class MapViewController: UIViewController {
 
+    let backButton = UIButton().then {
+        $0.setImage(#imageLiteral(resourceName: "back"), for: .normal)
+        $0.frame.size.width = 40
+        $0.frame.size.height = 40
+        $0.layer.cornerRadius = $0.frame.width / 2
+        $0.clipsToBounds = true
+        $0.addTarget(self, action: #selector(test), for: .touchUpInside)
+    }
+    let information = UIButton().then {
+        $0.setImage(#imageLiteral(resourceName: "information"), for: .normal)
+        $0.frame.size.width = 40
+        $0.frame.size.height = 40
+        $0.layer.cornerRadius = $0.frame.width / 2
+        $0.clipsToBounds = true
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
         let mapView = NMFMapView(frame: view.frame)
         view.addSubview(mapView)
+        layout()
+    }
+    
+    func layout() {
+        view.addSubview(backButton)
+        view.addSubview(information)
+        backButton.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(40)
+            $0.leading.equalToSuperview().offset(22)
+            $0.width.height.equalTo(40)
+        }
+        information.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(40)
+            $0.trailing.equalToSuperview().offset(-22)
+            $0.width.height.equalTo(40)
+        }
+    }
+    
+    @objc func test() {
+        
     }
 }
