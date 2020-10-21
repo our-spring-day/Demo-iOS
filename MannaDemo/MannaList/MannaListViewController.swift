7e17082864b7a5ff3d9add2c9091a404a9d94110
jeongjaein <wodls96@gmail.com>
2020-10-21 17:49:19 +0900

    feat : add bar button create

--- MannaDemo/MannaList/MannaListViewController.swift
+++ MannaDemo/MannaList/MannaListViewController.swift
@@ -6,11 +6,43 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class MannaListViewController: UIViewController {
-
+    
+    var navigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 0))
+    let createMannaButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(createMannaButtonAction))
+    let navItem = UINavigationItem(title: "Manna List")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .cyan
+        attribute()
+        layout()
+    }
+    
+    func attribute() {
+        self.do {
+            $0.title = "테스트"
+        }
+        navItem.do {
+            $0.rightBarButtonItem = createMannaButton
+        }
+        navigationBar.do {
+            $0.prefersLargeTitles = true
+            $0.setItems([navItem], animated: true)
+        }
+    }
+    
+    func layout() {
+        view.addSubview(navigationBar)
+        
+        navigationBar.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    @objc func createMannaButtonAction() {
+        
     }
 }
