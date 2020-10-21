4e8ce6670e104b292fb165f3d2418d636f68e4b4
jeongjaein <wodls96@gmail.com>
2020-10-21 17:30:39 +0900

    feat : MannaListViewController -> root 연결

---
+++ MannaDemo/MannaList/MannaListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MannaListViewController.swift
+//  MannaDemo
+//
+//  Created by 정재인 on 2020/10/21.
+//
+
+import UIKit
+
+class MannaListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+}
