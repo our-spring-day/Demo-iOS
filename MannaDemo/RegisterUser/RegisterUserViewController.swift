50b397569b064c7ce0a4a03c75f4cff7054b937f
s-doris <swdoriz@gmail.com>
2020-10-21 18:06:49 +0900

    feat : register user post api setting

--- MannaDemo/RegisterUser/RegisterUserViewController.swift
+++ MannaDemo/RegisterUser/RegisterUserViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 import Alamofire
 import SnapKit
+import SwiftKeychainWrapper
 import Then
 
 class RegisterUserViewController: UIViewController {
-
+    let url = "http://ec2-13-124-151-24.ap-northeast-2.compute.amazonaws.com:8888/user"
     let userName = UITextField().then {
         $0.layer.borderWidth = 1
         $0.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
@@ -31,7 +32,7 @@ class RegisterUserViewController: UIViewController {
         layout()
         
     }
-
+    
     func layout() {
         view.addSubview(userName)
         view.addSubview(completeButton)
@@ -51,6 +52,30 @@ class RegisterUserViewController: UIViewController {
     }
     
     @objc func complete() {
+        let view = UINavigationController(rootViewController: ViewController())
+        guard let deviceID = KeychainWrapper.standard.string(forKey: "device_id") else { return }
+        guard let userName = userName.text else { return }
         
+        let parameter: [String: String] = [
+            "device_id": deviceID,
+            "username": userName
+        ]
+        registerUser(parameter)
+        view.modalPresentationStyle = .fullScreen
+        present(view, animated: true, completion: nil)
+    }
+    
+    func registerUser(_ parameter: [String: String]) {
+        AF.request(url,
+                   method: .post,
+                   parameters: parameter,
+                   encoding: JSONEncoding.default).responseJSON { response in
+                    switch response.result {
+                        case .success(let value):
+                            print("\(value)")
+                        case .failure(let err):
+                            print("\(err)")
+                        }
+                   }
     }
 }
