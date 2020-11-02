3c67b0f9df98339bb6787e229387ecfa67263928
s-doris <swdoriz@gmail.com>
2020-11-02 21:27:02 +0900

    refactor : uiview extension 수정

--- MannaDemo/Extension/UIView+Ext.swift
+++ MannaDemo/Extension/UIView+Ext.swift
@@ -9,40 +9,9 @@ import UIKit
 
 extension UIView {
     func asImage() -> UIImage {
-        //        let renderer = UIGraphicsImageRenderer(size: self.bounds.size)
-        //        let image = renderer.image { _ in
-        //            self.drawHierarchy(in: self.bounds, afterScreenUpdates: true)
-        //        }
-        //        UIGraphicsBeginImageContextWithOptions(bounds.size, false, 0)
-        ////        drawHierarchy(in: bounds, afterScreenUpdates: true)
-        //        let image = UIGraphicsGetImageFromCurrentImageContext()
-        //        return image
-        //    }
-        
         let renderer = UIGraphicsImageRenderer(bounds: bounds)
-        return renderer.image {
-            layer.render(in: $0.cgContext)
+        return renderer.image { rendererContext in
+            layer.render(in: rendererContext.cgContext)
         }
-    }
-    func makeImageFrom(_ desiredView: UIView) -> UIImage {
-        let size = CGSize(width: desiredView.bounds.width, height: desiredView.bounds.height)
-        let renderer = UIGraphicsImageRenderer(size: size)
-        let image = renderer.image { (ctx) in
-            desiredView.drawHierarchy(in: CGRect(origin: .zero, size: size), afterScreenUpdates: true)
-        }
-        return image
-    }
-    
-    func createImage() -> UIImage {
-        
-        let rect: CGRect = self.frame
-        
-        UIGraphicsBeginImageContext(rect.size)
-        let context: CGContext = UIGraphicsGetCurrentContext()!
-        self.layer.render(in: context)
-        let img = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return img!
     }
 }
