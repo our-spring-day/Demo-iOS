9373ef2968b26dceb32fa0e702ba3755f0481572
jeongjaein <wodls96@gmail.com>
2020-11-03 20:55:27 +0900

    feat : present 커스텀 가즈아

--- MannaDemo/Map/ChatViewController.swift
+++ MannaDemo/Map/ChatViewController.swift
@@ -13,8 +13,19 @@ class ChatViewController: UIViewController {
     var nameLabel = UILabel()
     var contentLabel = UILabel()
     var paragraphStyle = NSMutableParagraphStyle()
+    let transitionManager = SomeTransitionManager()
+    lazy var tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapMessage))
     
-    fileprivate func attribute() {
+    @objc func tapMessage() {
+        let newView = tempViewController()
+        newView.transitioningDelegate = self
+        present(newView, animated: true)
+    }
+    func attribute() {
+        backgroundView.do {
+            $0.addGestureRecognizer(tapGesture)
+            $0.isUserInteractionEnabled = true
+        }
         profileImageView.do {
             $0.image = #imageLiteral(resourceName: "boddle")
         }
@@ -66,3 +77,92 @@ class ChatViewController: UIViewController {
         }
     }
 }
+extension ChatViewController: UIViewControllerTransitioningDelegate {
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        
+        return transitionManager
+    }
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return nil
+    }
+}
+
+class  SomeTransitionManager : UIPercentDrivenInteractiveTransition , UIViewControllerAnimatedTransitioning {
+    var duration2 = 0.8
+    var presenting = true
+    var originFrame = CGRect(x:  50, y:  UIScreen.main.bounds.height * 0.8, width: 0, height: 100)
+}
+
+extension SomeTransitionManager: UIViewControllerTransitioningDelegate{
+
+    private func animationController(fortPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+            return self
+        }
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+            return self
+        }
+    func interactionControllerForPresentation(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+            return nil
+        }
+    func interactionControllerForDismissal(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+            return self
+        }
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return duration2
+    }
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        
+        
+        let containerView = transitionContext.containerView
+        
+        //to View 는 띄워질 뷰컨의 뷰
+        let toView = transitionContext.view(forKey: .to)!
+        
+        let recipeView = presenting ? toView : transitionContext.view(forKey: .from)!
+        
+        let initialFrame = presenting ? originFrame : recipeView.frame
+        
+        let finalFrame = presenting ? recipeView.frame : originFrame
+
+        let xScaleFactor = presenting ?
+          initialFrame.width / finalFrame.width :
+          finalFrame.width / initialFrame.width
+
+        let yScaleFactor = presenting ?
+          initialFrame.height / finalFrame.height :
+          finalFrame.height / initialFrame.height
+        
+        let scaleTransform = CGAffineTransform(scaleX: xScaleFactor, y: yScaleFactor)
+
+        if presenting {
+          recipeView.transform = scaleTransform
+            
+          recipeView.center = CGPoint(
+            x: initialFrame.midX,
+            y: initialFrame.midY)
+          recipeView.clipsToBounds = true
+        }
+        recipeView.layer.cornerRadius = presenting ? 20.0 : 0.0
+        recipeView.layer.masksToBounds = true
+        
+        containerView.addSubview(toView)
+        containerView.bringSubviewToFront(recipeView)
+
+        UIView.animate(
+          withDuration: duration2,
+          delay:0.0,
+          usingSpringWithDamping: 0.5,
+          initialSpringVelocity: 0.2,
+          animations: {
+            recipeView.transform = self.presenting ? .identity : scaleTransform
+            recipeView.center = CGPoint(x: finalFrame.midX, y: finalFrame.midY)
+            recipeView.layer.cornerRadius = !self.presenting ? 20.0 : 0.0
+          },completion: { retult in
+            transitionContext.completeTransition(retult)
+          }
+        )
+    }
+}
+
+
+
---
+++ MannaDemo/Map/tempViewController.swift
@@ -0,0 +1,16 @@
+//
+//  tempViewController.swift
+//  MannaDemo
+//
+//  Created by 정재인 on 2020/11/03.
+//
+
+import UIKit
+
+class tempViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
