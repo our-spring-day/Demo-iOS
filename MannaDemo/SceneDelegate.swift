7ed6b1e5a1c98f5e7df48f9617db7b1a47292564
jeongjaein <wodls96@gmail.com>
2020-11-07 02:33:02 +0900

    픽스

--- MannaDemo/SceneDelegate.swift
+++ MannaDemo/SceneDelegate.swift
@@ -12,16 +12,16 @@ import SwiftKeychainWrapper
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     let url = "http://ec2-13-124-151-24.ap-northeast-2.compute.amazonaws.com:8888/user"
     var window: UIWindow?
-
+    
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             let mainView = MapViewController()
-    
+            
             let registerView = RegisterUserViewController()
-
+            
             if KeychainWrapper.standard.string(forKey: "device_id") == nil {
                 if let uuid = UIDevice.current.identifierForVendor?.uuidString {
                     let saveSuccessful: Bool = KeychainWrapper.standard.set(uuid, forKey: "device_id")
@@ -33,48 +33,49 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                     print(deviceID)
                     AF.request(url, method: .get, parameters: ["device_id": deviceID]).responseJSON { response in
                         switch response.result {
-                            case .success(let value):
-                                let json = value
-                                print("성공")
-                            case .failure(let _):
-                                print("유저 조회안됨")
-                            }
+                        case .success(let value):
+                            let json = value
+                            print("성공")
+                        case .failure(let _):
+                            print("유저 조회안됨")
+                        }
                     }
                 }
                 window.rootViewController = mainView
-            self.window = window
-            window.makeKeyAndVisible()
+                self.window = window
+                window.makeKeyAndVisible()
+            }
         }
+        
+        func sceneDidDisconnect(_ scene: UIScene) {
+            // Called as the scene is being released by the system.
+            // This occurs shortly after the scene enters the background, or when its session is discarded.
+            // Release any resources associated with this scene that can be re-created the next time the scene connects.
+            // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+        }
+        
+        func sceneDidBecomeActive(_ scene: UIScene) {
+            // Called when the scene has moved from an inactive state to an active state.
+            // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        }
+        
+        func sceneWillResignActive(_ scene: UIScene) {
+            // Called when the scene will move from an active state to an inactive state.
+            // This may occur due to temporary interruptions (ex. an incoming phone call).
+        }
+        
+        func sceneWillEnterForeground(_ scene: UIScene) {
+            // Called as the scene transitions from the background to the foreground.
+            // Use this method to undo the changes made on entering the background.
+        }
+        
+        func sceneDidEnterBackground(_ scene: UIScene) {
+            // Called as the scene transitions from the foreground to the background.
+            // Use this method to save data, release shared resources, and store enough scene-specific state information
+            // to restore the scene back to its current state.
+        }
+        
+        
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
+    
 }
-
