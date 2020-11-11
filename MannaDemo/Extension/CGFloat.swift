11edb1d76dc5382c3c72dca8934c514aecfd1fa1
jeongjaein <wodls96@gmail.com>
2020-11-11 13:31:40 +0900

    feat : 해상도 대응을 위한 CGFlat ext추가

---
+++ MannaDemo/Extension/CGFloat.swift
@@ -0,0 +1,13 @@
+//
+//  CGFloat.swift
+//  MannaDemo
+//
+//  Created by 정재인 on 2020/11/11.
+//
+
+import UIKit
+
+
+public extension CGFloat {
+    func pixelsToPoints() -> CGFloat { return self / UIScreen.main.scale }
+}
