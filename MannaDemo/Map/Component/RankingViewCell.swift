22c6ada45dd6d8ca8f72cd15040e93e23fcbc976
s-doris <swdoriz@gmail.com>
2020-11-11 13:42:16 +0900

    rankingViewCell init

--- MannaDemo/Map/Component/RankingViewCell.swift
+++ MannaDemo/Map/Component/RankingViewCell.swift
@@ -5,4 +5,26 @@
 //  Created by once on 2020/11/11.
 //
 
-import Foundation
+import UIKit
+
+class RankingViewCell: UITableViewCell {
+    let profileImage = UIImageView()
+    let userName = UILabel()
+    let state = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    func attribute() {
+        
+    }
+    
+    func layout() {
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
