483ef50728cca42b79256d6d17158568ed5c569c
jeongjaein <wodls96@gmail.com>
2020-11-13 18:16:39 +0900

    feat : 타이밍 이슈였다 해결

--- MannaDemo/Map/MapPresenter.swift
+++ MannaDemo/Map/MapPresenter.swift
@@ -17,13 +17,9 @@ class MapPresenter {
                     currentRanking[key]?.remainDistance = travelData.distance
                     print("이게 돌면서", currentRanking[key]?.remainDistance)
                     currentRanking[key]?.remainTime = travelData.duration
+                    completion(currentRanking)
                 }
             }
         }
-        currentRanking.keys.forEach { key in
-            print(currentRanking[key]?.name,currentRanking[key]?.remainDistance)
-        }
-        print("이게 돌고난 후 할당이 안되냐왜 씨ㅡ", currentRanking["0954A791-B5BE-4B56-8F25-07554A4D6684"]?.remainDistance)
-        completion(currentRanking)
     }
 }
