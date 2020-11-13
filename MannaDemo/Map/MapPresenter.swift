2a5542aef02597be22bdac4c4c28551a73d908df
jeongjaein <wodls96@gmail.com>
2020-11-13 15:33:57 +0900

    feat : mappresenter 생성

--- MannaDemo/Map/MapPresenter.swift
+++ MannaDemo/Map/MapPresenter.swift
@@ -8,9 +8,20 @@
 import Foundation
 
 class MapPresenter {
-    func currentRanking() {
-        
+    
+    func currentRanking(userList: [String : User], completion: @escaping ([User]) -> ()) {
+        var currentRanking = [User]()
+        UserModel.userList.keys.forEach { (key) in
+            MannaAPI.getPath(lat: UserModel.userList[key]!.latitude, lng: UserModel.userList[key]!.longitude) { (travelData) in
+                UserModel.userList[key]?.remainDistance = travelData.distance
+                UserModel.userList[key]?.remainTime = travelData.duration
+                
+            }
+        }
+        currentRanking = Array(UserModel.userList.values)
+        currentRanking.sort { $0.remainTime < $1.remainTime }
+//        userListForCollectionView.sort { $0.state && !$1.state}
+//        userListForCollectionView.sort { $0.remainTime < $1.remainTime }
+        completion(currentRanking)
     }
-    
-    
 }
