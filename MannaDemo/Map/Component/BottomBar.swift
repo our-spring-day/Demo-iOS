22a4d4ed2f199d11e7ad3e7593bbf37ac69780f0
jeongjaein <wodls96@gmail.com>
2020-11-19 16:48:58 +0900

    feat : create BottomBar

---
+++ MannaDemo/Map/Component/BottomBar.swift
@@ -0,0 +1,64 @@
+//
+//  ChatButton.swift
+//  MannaDemo
+//
+//  Created by 정재인 on 2020/11/19.
+//
+
+import UIKit
+
+class BottomBar: UIView {
+    var chatButton = UIButton()
+    var rankingBUtton = UIButton()
+    var timerView = TimerView(.mapView)
+    
+    func attribute() {
+        chatButton.do {
+            $0.backgroundColor = .white
+            $0.setImage(#imageLiteral(resourceName: "chat"), for: .normal)
+            $0.layer.cornerRadius = MannaDemo.convertHeight(value: 53) / 2
+            $0.layer.masksToBounds = true
+            $0.imageEdgeInsets = UIEdgeInsets(top: MannaDemo.convertHeight(value: 17), left: MannaDemo.convertHeight(value: 16.5), bottom: MannaDemo.convertHeight(value: 16), right: MannaDemo.convertHeight(value: 16.5))
+            $0.dropShadow()
+        }
+        rankingBUtton.do {
+            $0.backgroundColor = .white
+            $0.setImage(#imageLiteral(resourceName: "ranking"), for: .normal)
+            $0.layer.cornerRadius = MannaDemo.convertHeight(value: 53) / 2
+            $0.layer.masksToBounds = true
+            $0.imageEdgeInsets = UIEdgeInsets(top: MannaDemo.convertHeight(value: 15), left: MannaDemo.convertHeight(value: 14.5), bottom: MannaDemo.convertHeight(value: 14.5), right: MannaDemo.convertHeight(value: 14.5))
+            $0.dropShadow()
+        }
+    }
+    
+    func layout() {
+        [rankingBUtton, chatButton, timerView].forEach { addSubview($0) }
+        
+        rankingBUtton.snp.makeConstraints {
+            $0.bottom.equalTo(self.safeAreaLayoutGuide).offset(-34)
+            $0.leading.equalTo(chatButton.snp.trailing).offset(MannaDemo.convertWidth(value: 10))
+            $0.width.height.equalTo(MannaDemo.convertHeight(value: 54))
+        }
+        chatButton.snp.makeConstraints {
+            $0.bottom.equalTo(self.safeAreaLayoutGuide).offset(-34)
+            $0.leading.equalTo(self.snp.leading).offset(20)
+            $0.width.height.equalTo(MannaDemo.convertHeight(value: 54))
+        }
+        timerView.snp.makeConstraints {
+            $0.centerY.equalTo(chatButton.snp.centerY)
+            $0.trailing.equalTo(self).offset(-20)
+            $0.width.equalTo(MannaDemo.convertWidth(value: MannaDemo.convertWidth(value: 111)))
+            $0.height.equalTo(MannaDemo.convertHeight(value: MannaDemo.convertWidth(value: 48)))
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        attribute()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
